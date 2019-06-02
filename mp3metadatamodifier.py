59ce53228e1fb0453f5d7ab1bc4a602c6036b89b
Balaji Athreya <athreya86@gmail.com>
2019-06-02 15:34:32 -0700

    some bug fixes

--- mp3metadatamodifier.py
+++ mp3metadatamodifier.py
@@ -4,17 +4,17 @@ import shutil
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import ID3
 from mutagen.mp3 import MP3
-from mutagen.id3 import TALB,TPE1,TSOP,TPE2, TIT2,TCOM,TIT1,TSOT,TSO2,TSOC
+from mutagen.id3 import TALB,TPE1,TSOP,TPE2,TIT2,TCOM,TIT1,TSOT,TSO2,TSOC
 
 
 ## Modifies mp3/id3 metadata of mp3 files downloaded from Masstamilan.in and Masstamilan.org for cleaner look in iTunes/Apple Music
 # https://help.mp3tag.de/main_tags.html
 # https://www.programcreek.com/python/example/73822/mutagen.id3.APIC
 
-path = "/Users/balajiathreya/Downloads/newmusic"
+path = "/Users/balajiathreya/Downloads/music"
 
 
-#renames folders
+'''#renames folders
 
 albumfolders = os.listdir(path)
 for file_name in albumfolders:
@@ -45,7 +45,7 @@ for folder in albumfolders:
 				new_song = new_song.replace('-(','(').replace(')-',')').replace('--','').replace('-.mp3','.mp3')
 				print "mp3 is " + new_song
 				os.rename(os.path.join(song_folder, song), os.path.join(song_folder, new_song))
-
+'''
 
 #modify metadata 
 
@@ -54,102 +54,114 @@ for folder in albumfolders:
 	if os.path.isdir(os.path.join(path, folder)):
 		song_folder = os.path.join(path, folder)
 		#print 'song folder ' + song_folder
-		#if song_folder == '/Users/balajiathreya/Downloads/music/Punnagai-Mannan':
-		print "processing " + song_folder
-		songs = os.listdir(song_folder)
-		for song in songs:
-			if 'DS_Store' not in song :
-				print "======"+os.path.join(song_folder, song)+"======"
+		if song_folder == '/Users/balajiathreya/Downloads/music/Nandha':
+			print "processing " + song_folder
+			songs = os.listdir(song_folder)
+			for song in songs:
+				s = song.replace('.mp3','')
+				if 'DS_Store' not in song :
+					print "======"+os.path.join(song_folder, song)+"======"
 
-				id3 = ID3(os.path.join(song_folder, song))
+					mp3 = MP3(os.path.join(song_folder, song), ID3=EasyID3)
+					print "old mp3"
+					print (mp3.pprint())
 
-				# print "old id3"
-				# print id3
-				#COMMENT
-				id3.setall("COMM", "")
-				
-				#LYRICS
-				id3.setall("USLT", "")
-				
-				#workname
-				id3.setall('TIT1', "")
+					id3 = ID3(os.path.join(song_folder, song))
 
-				
-				#PIC
-				if id3.getall('APIC'):
-					id3.delall('APIC')
-
-				#ALBUM
-				talb = id3.getall('TALB')
-				if talb :
-					c = str(talb[0]).replace(' - [Masstamilan.In]','').replace('-[Masstamilan.In]','').replace(' [Masstamilan.In]','').replace(' [Masstamilan.in]','').replace(' - Masstamilan.In','').replace('[Masstamilan.in]','').replace(' - MassTamilan.com','').replace(' - Masstamilan.in','').replace(' -Masstamilan.In','').replace('Masstamilan.In','').replace('masstamilan.in','').replace(' (masstamilan.in)','').replace(' - MassTamilan.org','').replace('-StarMusiQ.Com','')
-					id3.add( TALB( encoding=3, text=c ) )
+					# print "old id3"
+					# print id3
+					#COMMENT
+					id3.setall("COMM", "")
 					
-				
-				#ARTIST
-				tpe1 = id3.getall('TPE1')
-				if tpe1 :
-					a = str(tpe1[0]).replace(' - [Masstamilan.In]','').replace('-[Masstamilan.In]','').replace(' [Masstamilan.In]','').replace(' [Masstamilan.in]','').replace(' - Masstamilan.In','').replace('[Masstamilan.in]','').replace(' - MassTamilan.com','').replace(' - Masstamilan.in','').replace(' -Masstamilan.In','').replace('Masstamilan.In','').replace('masstamilan.in','').replace(' (masstamilan.in)','').replace(' - MassTamilan.org','').replace('-StarMusiQ.Com','')
-					try:
-						id3.add( TPE1( encoding=3, text=a ) )
-						id3.add( TSOP( encoding=3, text=a ) )
-					except :
-						print "encoding error"
-				
-				#TITLE
-				tit2 = id3.getall('TIT2')
-				if tit2:
-					b = str(tit2[0]).replace(' - [Masstamilan.In]','').replace('-[Masstamilan.In]','').replace(' [Masstamilan.In]','').replace(' [Masstamilan.in]','').replace(' - Masstamilan.In','').replace('[Masstamilan.in]','').replace(' - MassTamilan.com','').replace(' - Masstamilan.in','').replace(' -Masstamilan.In','').replace('Masstamilan.In','').replace('masstamilan.in','').replace(' (masstamilan.in)','').replace(' - MassTamilan.org','').replace('-StarMusiQ.Com','')
-					try:
-						id3.add( TIT2( encoding=3, text=a ) )
-						id3.add( TSOT( encoding=3, text=a ) )
-					except :
-						print "encoding error"
+					#LYRICS
+					id3.setall("USLT", "")
+					
+					#workname
+					id3.setall('TIT1', "")
 
-				#ALBUMARTIST
-				tpe2 = id3.getall('TPE2')
-				if tpe2 : 
-					x = str(tpe2[0]).replace(' - [Masstamilan.In]','').replace('-[Masstamilan.In]','').replace(' [Masstamilan.In]','').replace(' [Masstamilan.in]','').replace(' - Masstamilan.In','').replace('[Masstamilan.in]','').replace(' - MassTamilan.com','').replace(' - Masstamilan.in','').replace(' -Masstamilan.In','').replace('Masstamilan.In','').replace('masstamilan.in','').replace(' (masstamilan.in)','').replace(' - MassTamilan.org','').replace('-StarMusiQ.Com','')
-					try:
-						id3.add( TPE2( encoding=3, text=a ) )
-						id3.add( TSO2( encoding=3, text=a ) )
-					except :
-						print "encoding error"
+					
+					#PIC
+					if id3.getall('APIC'):
+						id3.delall('APIC')
 
-				# composer 
-				tcom = id3.getall('TCOM')
-				if tcom:
-					try:
-						id3.add( TCOM( encoding=3, text=a ) )
-						id3.add( TSOC( encoding=3, text=a ) )
-					except :
-						print "encoding error"
+					#ALBUM
+					talb = id3.getall('TALB')
+					if talb :
+						c = str(talb[0]).replace(' - [Masstamilan.In]','').replace('-[Masstamilan.In]','').replace(' [Masstamilan.In]','').replace(' [Masstamilan.in]','').replace(' - Masstamilan.In','').replace('[Masstamilan.in]','').replace(' - MassTamilan.com','').replace(' - Masstamilan.in','').replace(' -Masstamilan.In','').replace('Masstamilan.In','').replace('masstamilan.in','').replace(' (masstamilan.in)','').replace(' - MassTamilan.org','').replace('-StarMusiQ.Com','')
+						id3.add( TALB( encoding=3, text=c ) )
 
-				id3.save(v2_version=3)
+					#ALBUMARTIST
+					tpe2 = id3.getall('TPE2')
+					if tpe2 : 
+						x = str(tpe2[0]).replace(' - [Masstamilan.In]','').replace('-[Masstamilan.In]','').replace(' [Masstamilan.In]','').replace(' [Masstamilan.in]','').replace(' - Masstamilan.In','').replace('[Masstamilan.in]','').replace(' - MassTamilan.com','').replace(' - Masstamilan.in','').replace(' -Masstamilan.In','').replace('Masstamilan.In','').replace('masstamilan.in','').replace(' (masstamilan.in)','').replace(' - MassTamilan.org','').replace('-StarMusiQ.Com','')
+						try:
+							id3.add( TPE2( encoding=3, text=x ) )
+							id3.add( TSO2( encoding=3, text=x ) )
+						except :
+							print "***encoding error***"
 
-				# print "new id3"
-				# id3 = ID3(os.path.join(song_folder, song))
-				# print id3
+					# composer 
+					tcom = id3.getall('TCOM')
+					if tcom:
+						try:
+							id3.add( TCOM( encoding=3, text=x ) )
+							id3.add( TSOC( encoding=3, text=x ) )
+						except :
+							print "***encoding error***"
+						
+					
+					#ARTIST
+					tpe1 = id3.getall('TPE1')
+					if tpe1 :
+						a = str(tpe1[0]).replace(' - [Masstamilan.In]','').replace('-[Masstamilan.In]','').replace(' [Masstamilan.In]','').replace(' [Masstamilan.in]','').replace(' - Masstamilan.In','').replace('[Masstamilan.in]','').replace(' - MassTamilan.com','').replace(' - Masstamilan.in','').replace(' -Masstamilan.In','').replace('Masstamilan.In','').replace('masstamilan.in','').replace(' (masstamilan.in)','').replace(' - MassTamilan.org','').replace('-StarMusiQ.Com','')
+						try:
+							id3.add( TPE1( encoding=3, text=a ) )
+							id3.add( TSOP( encoding=3, text=a ) )
+						except :
+							print "***encoding error***"
+					
+					#TITLE
+					tit2 = id3.getall('TIT2')
+					if tit2:
+						if tit2 == tpe1:
+							try:
+								id3.add( TIT2( encoding=3, text=s ) )
+								id3.add( TSOT( encoding=3, text=s ) )
+							except :
+								print "***encoding error***"
+						else:
+							b = str(tit2[0]).replace(' - [Masstamilan.In]','').replace('-[Masstamilan.In]','').replace(' [Masstamilan.In]','').replace(' [Masstamilan.in]','').replace(' - Masstamilan.In','').replace('[Masstamilan.in]','').replace(' - MassTamilan.com','').replace(' - Masstamilan.in','').replace(' -Masstamilan.In','').replace('Masstamilan.In','').replace('masstamilan.in','').replace(' (masstamilan.in)','').replace(' - MassTamilan.org','').replace('-StarMusiQ.Com','')
+							try:
+								id3.add( TIT2( encoding=3, text=b ) )
+								id3.add( TSOT( encoding=3, text=b ) )
+							except :
+								print "***encoding error***"
 
-				mp3 = MP3(os.path.join(song_folder, song), ID3=EasyID3)
-				print (mp3.pprint())
-				
-				mp3['copyright'] = ""
-				mp3['genre'] = ""
-				mp3['arranger'] = ""
-				mp3['conductor'] = ""
-				mp3['version'] = ''
-				mp3['lyricist'] = ''
-				mp3['encodedby'] = ''
-				mp3['organization'] = ''
-				mp3['isrc'] = ''
-				mp3['bpm'] = ''
-				mp3['website'] = ''
-				mp3.save(v2_version=3)
 
-				newmp3 = MP3(os.path.join(song_folder, song), ID3=EasyID3)
-				print "new mp3"
-				print (newmp3.pprint())
+					id3.save(v2_version=3)
+
+					# print "new id3"
+					# id3 = ID3(os.path.join(song_folder, song))
+					# print id3
+
+					if tit2 == tpe1:
+						mp3['title'] = s;
+					mp3['copyright'] = ""
+					mp3['genre'] = ""
+					mp3['arranger'] = ""
+					mp3['conductor'] = ""
+					mp3['version'] = ''
+					mp3['lyricist'] = ''
+					mp3['encodedby'] = ''
+					mp3['organization'] = ''
+					mp3['isrc'] = ''
+					mp3['bpm'] = ''
+					mp3['website'] = ''
+					mp3.save(v2_version=3)
+
+					newmp3 = MP3(os.path.join(song_folder, song), ID3=EasyID3)
+					print "new mp3"
+					print (newmp3.pprint())
 
 
 		 			
